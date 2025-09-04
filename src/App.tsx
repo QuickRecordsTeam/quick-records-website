51c99648190272209a6b4d6ebaa13fb0c13464bd
Suh-Edmond <suhedmond25@yahoo.com>
2025-09-04 13:33:33 +0100

    added lazy loading of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import { Fragment } from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import CallToAction from "./components/CallToAction";
-import FAQ from "./components/FAQ";
-import Testimonials from "./components/Testimonials";
-import Contact from "./components/Contact";
-import VideoCaption from "./components/VideoCaption";
+import { Fragment, lazy, Suspense } from "react";
 
+const Header = lazy(() => import("./components/Header"));
+const Footer = lazy(() => import("./components/Footer"));
+const Hero = lazy(() => import("./components/Hero"));
+const Features = lazy(() => import("./components/Features"));
+const VideoCaption = lazy(() => import("./components/VideoCaption"));
+const CallToAction = lazy(() => import("./components/CallToAction"));
+const FAQ = lazy(() => import("./components/FAQ"));
+const Testimonials = lazy(() => import("./components/Testimonials"));
+const Contact = lazy(() => import("./components/Contact"));
 function App() {
   return (
     <Fragment>
-      <Header />
-      <Hero />
-      <Features />
-      <VideoCaption />
-      <CallToAction />
-      <FAQ />
-      <Testimonials />
-      <Contact />
-      <Footer />
+      <Suspense fallback={<div id="preloader"></div>}>
+        <Header />
+        <Hero />
+        <Features />
+        <VideoCaption />
+        <CallToAction />
+        <FAQ />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </Suspense>
     </Fragment>
   );
 }
